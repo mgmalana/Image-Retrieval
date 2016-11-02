2b05abe9c29aa034592146351566c80681641620
katelacsamana <katelacsamana@gmail.com>
2016-11-02 14:18:20 +0800

    Added generateHTML method to view results

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@ import model.Image;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
@@ -23,11 +25,31 @@ public class Main {
         return chooser.getSelectedFile();
     }
 
+    public static void generateHTMLResults(File selectedFile, File [] similarImages){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write("<html>");
+            bw.write("<body>");
+            bw.write("<h2>Query: <h2><br>");
+            bw.write("<img src=\"" + selectedFile.getPath() + "\"</img><br>");
+            bw.write("<h2>Top " +  similarImages.length + "similar images found: </h2><br>");
+            for (File f: similarImages){
+                bw.write("<img src=\"" + f.getPath() + "\"</img><hr>");
+                //System.out.print(f.getName() +", ");
+            }
+            bw.write("</body>");
+            bw.write("</html>");
+            bw.close();
+        } catch (IOException e){
+
+        }
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
         //File selectedFile = main.initFileChooser();
 
-        File selectedFile = new File("/Users/mgmalana/Documents/Image-Retrieval/images/0.jpg");
+        File selectedFile = new File("/Users/Kate/IdeaProjects/Image-Retrieval/images/0.jpg");
 
         try {
 
@@ -38,10 +60,11 @@ public class Main {
                 File[] similarImages = imageRetrieve.getSimilarImages(queryImage, selectedFile.getParentFile(),
                         ImageRetrieve.imageRetrieval.COLORHISTOGRAM);
 
-                System.out.println("Top " + similarImages.length + " similar images are ");
-                for (File file: similarImages){
-                    System.out.print(file.getName() +", ");
-                }
+//                System.out.println("Top " + similarImages.length + " similar images are ");
+//                for (File file: similarImages){
+//                    System.out.print(file.getName() +", ");
+//                }
+                generateHTMLResults(selectedFile, similarImages);
 
             } else {
                 return;
